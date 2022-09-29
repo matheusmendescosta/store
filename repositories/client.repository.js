13f4b2d11d955dc12529c8ade45f3ef6d649b6a2
Matheus Mendes <matheus_mendess@live.com>
2022-09-28 22:37:55 -0300

    get clients

--- repositories/client.repository.js
+++ repositories/client.repository.js
@@ -2,12 +2,69 @@ import { connect } from "./db.js";
 
 async function insertClient(client) {
   const conn = await connect();
-  const sql = "INSERT INTO clients (name, cpf, phone, email, address) VALUES ($1, $2, $3, $4, $5) RETURNING *";
-  const values = [client.name, client.cpf, client.phone, client.email, client.address];
-  const res = await conn.query(sql, values);
-  return res.rows[0];
+
+  try {
+    const sql = "INSERT INTO clients (name, cpf, phone, email, address) VALUES ($1, $2, $3, $4, $5) RETURNING *";
+    const values = [client.name, client.cpf, client.phone, client.email, client.address];
+    const res = await conn.query(sql, values);
+    return res.rows[0];
+  } catch (error) {
+    throw error;
+  } finally {
+    conn.release();
+  }
+}
+
+async function getClients() {
+  const conn = await connect();
+
+  try {
+    const res = await conn.query("SELECT * FROM clients");
+    return res.rows;
+  } catch (error) {
+    throw error;
+  } finally {
+    conn.release();
+  }
+}
+
+async function getClient() {
+  const conn = await connect();
+
+  try {
+  } catch (error) {
+    throw error;
+  } finally {
+    conn.release();
+  }
+}
+
+async function updateClient() {
+  const conn = await connect();
+
+  try {
+  } catch (error) {
+    throw error;
+  } finally {
+    conn.release();
+  }
+}
+
+async function deleteClient() {
+  const conn = await connect();
+
+  try {
+  } catch (error) {
+    throw error;
+  } finally {
+    conn.release();
+  }
 }
 
 export default {
   insertClient,
+  getClients,
+  getClient,
+  updateClient,
+  deleteClient,
 };
