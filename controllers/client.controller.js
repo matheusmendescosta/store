79e82080f303e513c4c4457bd8528e8dd2617ed9
matheus mendes <matheus_mendess@live.com>
2022-10-04 09:04:37 -0300

    CRUD de cliente completo

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -6,7 +6,8 @@ async function createClient(req, res, next) {
     if (!client.name || !client.cpf || !client.phone || !client.email || !client.address) {
       throw new Error("Todos os campos são obrigatorios: nome, cpf, phone, email, address");
     }
-    res.send(await clientService.createClient(client));
+    client = await clientService.createClient(client);
+    res.send(client);
     logger.info(`POST /client - ${JSON.stringify(client)}`);
   } catch (error) {
     next(error);
@@ -22,7 +23,42 @@ async function getClients(req, res, next) {
   }
 }
 
+async function getClient(req, res, next) {
+  try {
+    res.send(await clientService.getClient(req.params.id));
+    logger.info("GET /client");
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function deleteClient(req, res, next) {
+  try {
+    await clientService.deleteClient(req.params.id);
+    res.end();
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function updateClient(req, res, next) {
+  try {
+    let client = req.body;
+    if (!client.client_id || !client.name || !client.cpf || !client.phone || !client.email || !client.address) {
+      throw new Error("Todos os campos são obrigatorios: nome, cpf, phone, email, address");
+    }
+    client = await clientService.updateClient(client);
+    res.send(client);
+    logger.info(`PUT /client - ${JSON.stringify(client)}`);
+  } catch (error) {
+    next(error);
+  }
+}
+
 export default {
   createClient,
   getClients,
+  getClient,
+  deleteClient,
+  updateClient,
 };
--- repositories/client.repository.js
+++ repositories/client.repository.js
@@ -28,10 +28,12 @@ async function getClients() {
   }
 }
 
-async function getClient() {
+async function getClient(id) {
   const conn = await connect();
 
   try {
+    const res = await conn.query("SELECT * FROM clients WHERE client_id = $1", [id]);
+    return res.rows[0];
   } catch (error) {
     throw error;
   } finally {
@@ -39,10 +41,11 @@ async function getClient() {
   }
 }
 
-async function updateClient() {
+async function deleteClient(id) {
   const conn = await connect();
 
   try {
+    await conn.query("DELETE FROM clients WHERE client_id = $1", [id]);
   } catch (error) {
     throw error;
   } finally {
@@ -50,10 +53,15 @@ async function updateClient() {
   }
 }
 
-async function deleteClient() {
+async function updateClient(client) {
   const conn = await connect();
 
   try {
+    const sql =
+      "UPDATE clients SET name = $1, cpf = $2, phone = $3, email = $4, address = $5 WHERE client_id = $6 RETURNING *";
+    const values = [client.name, client.cpf, client.phone, client.email, client.address, client.client_id];
+    const res = await conn.query(sql, values);
+    return res.rows[0];
   } catch (error) {
     throw error;
   } finally {
